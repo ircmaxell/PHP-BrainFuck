05cec05a6e3ac140d4cb0b94f1acba41e31e5842
Anthony Ferrara <ircmaxell@gmail.com>
2012-12-22 21:51:53 -0500

    Working Minimum, No Loops or Conditionals

--- lib/BrainFuck/Language.php
+++ lib/BrainFuck/Language.php
@@ -5,6 +5,43 @@ namespace BrainFuck;
 class Language {
 
     public function run($program, array $input = array()) {
-        return array(1);
+        $output = array();
+        $memory = array(0);
+        $memoryPos = 0;
+        $inputPos = 0;
+
+        $program = $this->parse($program);
+
+        foreach ($program as $op) {
+            switch ($op) {
+                case ',':
+                    $memory[$memoryPos] = $input[$inputPos++];
+                    break;
+                case '.':
+                    $output[] = $memory[$memoryPos];
+                    break;
+                case '+':
+                    $memory[$memoryPos]++;
+                    break;
+                case '-':
+                    $memory[$memoryPos]--;
+                    break;
+                case '>':
+                    $memoryPos++;
+                    break;
+                case '<':
+                    $memoryPos--;
+                    break;
+            }
+            if (!isset($memory[$memoryPos])) {
+                $memory[$memoryPos] = 0;
+            }
+        }
+        return $output;
     }
+
+    protected function parse($program) {
+        return str_split($program);
+    }
+
 }
--- test/BrainFuck/LanguageTest.php
+++ test/BrainFuck/LanguageTest.php
@@ -7,6 +7,10 @@ class LanguageTest extends \PHPUnit_Framework_TestCase {
     public static function provideTestProgram() {
         return array(
             array(',+.', array(0), array(1)),
+            array(',+.', array(1), array(2)),
+            array(',-.', array(1), array(0)),
+            array(',>++<.', array(1), array(1)),
+            array(',>,>+.<.<.', array(3, 2), array(1, 2, 3)),
         );
     }
 
